bce5cfe421c547d7a8660ccb7a2f710ff9460b72
Fabio Ramos Alves <fabio.ramos.alves@br.infra.d>
2022-10-19 09:13:07 -0300

    incluindo os comandos para geração da imagem no Docker Hub

--- 6_kubernetes/1_projeto/app.py
+++ 6_kubernetes/1_projeto/app.py
@@ -5,8 +5,8 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/')
-def index()
+def index():
     return render_template('index.html')
 
 if __name__ == "__main__" :
-    app.run(host="0.0.0.0", debug=True, port = "5000")
+    app.run(host="0.0.0.0", debug=True, port="5000")
